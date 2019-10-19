4a3248643a260f5bca75e155a7e354748d9f8701
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2019-10-18 20:51:51 -0500

    Criado validações para os horários de agendiemntos.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,14 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
 class AppointmentController {
   async store(req, res) {
+    /**
+     * Validate user input data.
+     */
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
       date: Yup.date().required(),
@@ -27,12 +32,36 @@ class AppointmentController {
     }
 
     /**
+     * Check for past dates.
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(422).json({ error: 'Past dates are not permitted.' });
+    }
+
+    /**
+     * Check date availability.
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(422).json({ error: 'Appointment date is not available.' });
+    }
+
+    /**
      * Create appointment.
      */
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);
