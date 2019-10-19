f50298fd4f8c1689ed7fc092735572f50be40901
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2019-10-18 23:10:17 -0500

    Notificando novos agendamentos.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -89,6 +91,21 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    /**
+     * Notify appointment provider.
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: ptBR },
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${ user.name } para ${ formattedDate }`,
+      user: provider_id,
     });
 
     return res.json(appointment);
