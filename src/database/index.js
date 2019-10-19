93b517bae316d569361f8b19a86b758303233a9a
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2019-10-18 20:11:12 -0500

    Adicionado a migration e o model de agendamento.

--- src/database/index.js
+++ src/database/index.js
@@ -4,8 +4,9 @@ import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
---
+++ src/database/migrations/20191019010044-create-appointments.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('appointments', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    user_id: {
+      type: Sequelize.INTEGER,
+      references: { model: 'users', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    },
+    provider_id: {
+      type: Sequelize.INTEGER,
+      references: { model: 'users', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    },
+    canceled_at: {
+      type: Sequelize.DATE,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  down: queryInterface => queryInterface.dropTable('appointments'),
+};
