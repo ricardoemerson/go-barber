cd56be6787155cc1c44799e70262aba87ca85841
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2019-10-18 23:22:23 -0500

    Listando notificações do usuário.

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,26 @@
+import Notification from '../schemas/Notification';
+import User from '../models/User';
+
+
+class NotificationController {
+  async index(req, res) {
+    /**
+     * Check if provider_id is a provider.
+     */
+    const checkIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res.status(422).json({ error: 'Only providers can load notifications.' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    }).sort({ createdAt: 'desc' }).limit(20);
+
+    return res.json(notifications);
+  }
+}
+
+export default new NotificationController();
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,16 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
-import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -29,6 +31,8 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
