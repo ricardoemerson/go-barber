62e3bc7feda5d978e2f1b3a15d8962b36c929c0c
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2019-10-18 21:25:53 -0500

    Listando agenda do prestador.

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,41 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class ScheduleController {
+  async index(req, res) {
+    /**
+     * Check if user is a provider.
+     */
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(422).json({ error: 'User is not a provider.' });
+    }
+
+    /**
+     * List appointments betwenn start and end of day.
+     */
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
