fd2ad5930ea28fc22527a639463cdb137dfd64d8
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2019-10-20 11:24:14 -0500

    Campos virtuais no agendamento.

---
+++ Procfile
@@ -0,0 +1,2 @@
+dev: yarn dev
+queue: yarn queue
--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -17,7 +17,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
